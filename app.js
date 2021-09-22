57a8973abff285f3951bb831be59b3dd7edffdcb
Guy Effam <bxljobmail@gmail.com>
2021-09-22 17:46:37 +0200

    ending code but not working

--- app.js
+++ app.js
@@ -3,41 +3,112 @@ document.addEventListener('DOMContenLoaded', () =>{
     //card options
     const cardArray = [
         {
-            name:'fries',
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+          },
+          {
+            name: 'pizza',
+            img: 'images/pizza.png'
+          },
+          {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+          },
+          {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+          },
+          {
+            name: 'fries',
             img: 'images/fries.png'
-        },
-        {
-            name:'cheeseburger',
+          },
+          {
+            name: 'cheeseburger',
             img: 'images/cheeseburger.png'
-        }
+          },
+          {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+          },
+          {
+            name: 'pizza',
+            img: 'images/pizza.png'
+          },
+          {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+          },
+          {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+          }
+        
     ]
 
-    const grid = document.querySelector('.grid')
-    var cardsChosen = []
-    var cardsChosenId = []
+    cardArray.sort(() => 0.5 - Math.random())
 
-    //create your boead
-    function createBorad() {
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = []
+
+    //create your board
+    function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
-            var card = document.createElement('img')
+            const card = document.createElement('img')
             card.setAttribute('src', 'images/blank.png')
             card.setAttribute('data-id', i)
-            card.addEventListener('click', flipcard)
+            card.addEventListener('click', flipCard)
             grid.appendChild(card)
         }
     }
 
     
     //check for matches
+    function checkForMatch() {
+        const cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        
+        if(cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            cardsWon.push(cardsChosen)
+        }
+        else if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'images/white.png')
+            cards[optionTwoId].setAttribute('src', 'images/white.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+          } else {
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            alert('Sorry, try again')
+          }
+          cardsChosen = []
+          cardsChosenId = []
+          resultDisplay.textContent = cardsWon.length
+          if  (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+          }
+        }
 
     //flip your card
-    function flipcard() {
-        var cardId = this.getAttribute('data-id')
-        cardsChosen.push(cardArray[cardId.name])
+    function flipCard() {
+       let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
         cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if(cardsChosen.length ===2) {
+            setTimeout(checkForMatch, 500)
+        }
     }
 
 
-    createBorad()
+    createBoard()
 
 }) 
